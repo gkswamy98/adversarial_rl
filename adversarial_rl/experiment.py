52d1d08aca739f894717b02dbab1dcff09a2fd26
Noah Golmant <noah.golmant@gmail.com>
2019-03-31 20:12:28 -0700

    simplify things a lot; just need to add logits extraction from the model

--- adversarial_rl/experiment.py
+++ adversarial_rl/experiment.py
@@ -1,65 +1,56 @@
 """ This contains the code to attack an arbitrary trained agent on a gym env"""
 import track
 from cleverhans.attacks import FastGradientMethod
-from dqn import learn as dqn_learner
-from trpo import learn as trpo_learner
+from cleverhans.loss import CrossEntropyLoss, Loss
 
-from baselines.common.vec_env import VecFrameStack, VecNormalize, VecEnv
-from baselines.common.vec_env.vec_video_recorder import VecVideoRecorder
-from baselines.run import build_env, get_learn_function_defaults,\
-                          get_default_network, get_env_type
+import baselines
+from baselines.common.vec_env import VecEnv
+from baselines.common.tf_util import save_variables
 
 import os
 import numpy as np
-
-_LEARNERS = {
-    'dqn': dqn_learner,
-    'trpo': trpo_learner
-}
 
 _ATTACKS = {
     'fgsm': FastGradientMethod,
 }
 
 
-def _train(args):
-    args_dict = vars(args)
-    env_type, env_id = get_env_type(args)
-    print('env_type: {}'.format(env_type))
-
-    total_timesteps = int(args.num_timesteps)
-    seed = args.seed
-
-    # create the learner
-    learn = _LEARNERS[args.alg]
-    attack = _ATTACKS[args.attack_method]  # can't init, it needs the model
-
-    env = build_env(args)
-    if args.save_video_interval != 0:
-        env = VecVideoRecorder(env, os.join(track.trial_dir(), "videos"),
-                               record_video_trigger=lambda x: x
-                               % args.save_video_interval == 0,
-                               video_length=args.save_video_length)
-
-    model = learn(
-        attack=attack,
-        env=env,
-        seed=seed,
-        total_timesteps=total_timesteps,
-        **args_dict
-    )
-    return model, env
+def _load(args):
+    setattr(args, 'num_timesteps', 0)
+    extra_args = {}  # TODO see if we need this from baselines..run
+    # try to load with pattern matching (this makes grid search easier)
+    if args.load_path == '':
+        args.load_path = './%s_%s' % (args.alg, args.env)
+    return baselines.train(args, extra_args)
 
 
-def eval_model(model, env, eval_steps=10):
+class WrappedModel:
+    """ cleverhans expected a get_logits function for the model """
+    def __init__(self, model):
+        self.model = model
+
+    def get_logits(x, **kwargs):
+        # get logits from the model for this action ....
+        pass
+
+
+def eval_model(model, env, attack_method, eval_steps=10, **attack_params):
     obs = env.reset()
     state = model.initial_state if hasattr(model, 'initial_state') else None
     dones = np.zeros((1,))
 
+    cleverhans_model = WrappedModel(model)
+    attack = _ATTACKS[attack_method](model)
+
     episode_rew = 0
     for _ in range(eval_steps):
         if state is not None:
-            actions, _, state, _ = model.step(obs, S=state, M=dones)
+            # perform the attack!
+            loss = CrossEntropyLoss(model, attack=attack)
+            logits = cleverhans_model.get_logits(state)
+            adv_state = attack.generate(state, **attack_params)
+
+            actions, _, state, _ = model.step(obs, S=adv_state, M=dones)
         else:
             actions, _, _, _ = model.step(obs)
 
@@ -76,11 +67,16 @@ def eval_model(model, env, eval_steps=10):
 
 
 def main(args):
-    # track will already handle logging structures
-    model, env = _train(args)
-    episode_reward = eval_model(model, env, eval_steps=args.eval_steps)
+    if args.train:
+        model = baselines.main(args)
+        fname = './%s_%s' % (args.alg, args.env)
+        save_variables(fname)
+        return
+
+    model, env = _load(args)
+    episode_reward = eval_model(model, env, eval_steps=args.eval_steps,
+                                attack=args.attack,
+                                eps=args.eps)
     model_path = os.path.join(track.trial_dir(), 'model.ckpt')
     track.metric(trial_id=args.trial_id, reward=episode_reward,
                  model_path=model_path)
-    # TODO dump model here
-    trial_dir = os.path.join(args.logdir, 'trial_%d' % args.trial_id)
--- adversarial_rl/run.py
+++ adversarial_rl/run.py
@@ -15,17 +15,24 @@ def _logdir_name(args):
 
 def add_args(parser):
     # add the rest....
-    parser.add_argument('alg', default='dqn', help='agent to train')
-    parser.add_argument('attack', default='fgsm',
+    parser.add_argument('--alg', default='deepq', help='agent to train',
+                        choices=['deepq', 'mpi_trpo'])
+    parser.add_argument('--env', default='PongNoFrameskip-v4',
+                        help='Gym environment name to train on')
+    parser.add_argument('--attack', default='fgsm',
                         choices=['fgsm'],
                         help='attack method to run')
-    parser.add_argument('attack-norm', default='l1',
+    parser.add_argument('--attack-norm', default='l1',
                         choices=['l1', 'l2', 'l0'],
                         help="norm we use to constrain perturbation size")
-    parser.add_argument('eps', default=.1, type=float,
+    parser.add_argument('--eps', default=.1, type=float,
                         help='perturbation magnitude')
-    parser.add_argument('num-trials', default=10, type=int,
+    parser.add_argument('--num-trials', default=10, type=int,
                         help='how many times to repeat the experiment')
+    parser.add_argument('--load_path', default='', required=True,
+                        help='Location of model with correct policy')
+    parser.add_argument('--train', action='store_const',
+                        help='if true, just trains the plain model from scratch')
 
 
 if __name__ == '__main__':
